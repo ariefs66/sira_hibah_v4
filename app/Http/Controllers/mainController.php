19122983ff2444a6238e4317bf03906ae89fa32c
Willy Muhammad <willymuhammad@gmail.com>
2017-09-07 21:39:27 +0700

    REMOVE !2018

--- app/Http/Controllers/mainController.php
+++ app/Http/Controllers/mainController.php
@@ -62,19 +62,19 @@ class mainController extends Controller
 		$data  = TahunAnggaran::all();
 		$view 	= '';
 		foreach($data as $data){
-			if($data->TAHUN != '2018'){
+			// if($data->TAHUN != '2018'){
 				if($tahun == $data->TAHUN and $status == $data->STATUS){
 				$view 	.= '<option value="'.$data->TAHUN.'/'.$data->STATUS.'" id="o'.$data->TAHUN.$data->STATUS.'" selected>'.$data->TAHUN.'-'.$data->STATUS.'</option>';
 				}else{
 				$view 	.= '<option value="'.$data->TAHUN.'/'.$data->STATUS.'" id="o'.$data->TAHUN.$data->STATUS.'">'.$data->TAHUN.'-'.$data->STATUS.'</option>';
 				}
-			}else if (Auth::user()->level==8 || Auth::user()->email=='MONITORING'){
-				if($tahun == $data->TAHUN and $status == $data->STATUS){
-				$view 	.= '<option value="'.$data->TAHUN.'/'.$data->STATUS.'" id="o'.$data->TAHUN.$data->STATUS.'" selected>'.$data->TAHUN.'-'.$data->STATUS.'</option>';
-				}else{
-				$view 	.= '<option value="'.$data->TAHUN.'/'.$data->STATUS.'" id="o'.$data->TAHUN.$data->STATUS.'">'.$data->TAHUN.'-'.$data->STATUS.'</option>';
-				}
-			}
+			// }else if (Auth::user()->level==8 || Auth::user()->email=='MONITORING'){
+			// 	if($tahun == $data->TAHUN and $status == $data->STATUS){
+			// 	$view 	.= '<option value="'.$data->TAHUN.'/'.$data->STATUS.'" id="o'.$data->TAHUN.$data->STATUS.'" selected>'.$data->TAHUN.'-'.$data->STATUS.'</option>';
+			// 	}else{
+			// 	$view 	.= '<option value="'.$data->TAHUN.'/'.$data->STATUS.'" id="o'.$data->TAHUN.$data->STATUS.'">'.$data->TAHUN.'-'.$data->STATUS.'</option>';
+			// 	}
+			// }
 		}
 		return $view;
 	}
