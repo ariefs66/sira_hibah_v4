e38aa60cf3971b24520d13341a5295b6bf99cd85
Erwin Hari Praseno <erwinhape@gmail.com>
2018-01-12 23:59:41 +0700

    CEK

--- resources/views/budgeting/lampiran/apbd1.blade.php
+++ resources/views/budgeting/lampiran/apbd1.blade.php
@@ -235,7 +235,7 @@
 	</tr>
 	<tr>
 		<td class="border-rincian"></td>
-		<td class="border-rincian kanan"><b>SURPLUS/(DEFISIT)</b></td>
+		<td class="border-rincian kanan"><b>SURPLUS/(DEFISIT)s</b></td>
 		<td class="border kanan total"><b>
 		@if((float)$pendapatan-($bl+$btl1+$btl2+$btl3+$btl4+$btl5))<0)
 		({{ number_format(abs((float)$pendapatan-($bl+$btl1+$btl2+$btl3+$btl4+$btl5)), 2, ',', '.') }})
