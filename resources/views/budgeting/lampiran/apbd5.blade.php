955a0d59d81043974dcb6b4e9ffec08735967794
Erwin Hari Praseno <erwinhape@gmail.com>
2018-01-17 16:53:54 +0700

    CEK LAMPIRAN 5 PERDA

--- resources/views/budgeting/lampiran/apbd5.blade.php
+++ resources/views/budgeting/lampiran/apbd5.blade.php
@@ -164,7 +164,7 @@
         <td class="border text_blok kanan">{{ number_format($totalpegawaimurni,2,',','.') }}</td>
         <td class="border text_blok kanan">{{ number_format($totaljasamurni+0.01,2,',','.') }}</td>
         <td class="border text_blok kanan">{{ number_format($totalmodalmurni+0.01,2,',','.') }}</td>
-        <td class="border text_blok kanan">{{ number_format(($totalgajimurni+$totalnongajimurni+$totalpegawaimurni+$totaljasamurni+$totalmodalmurni),2,',','.') }}</td>
+        <td class="border text_blok kanan">{{ number_format(($totalgajimurni+$totalnongajimurni+$totalpegawaimurni+$totaljasamurni+$totalmodalmurni)+0.01,2,',','.') }}</td>
     </tr>
 	
 	</tbody>	
