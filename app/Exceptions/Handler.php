41871775171284857d32fa2d65fd3319408d3ff0
Arief Sulaeman <sulaemanarief380@gmail.com>
2018-03-05 15:57:39 +0700

    Update Error Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -44,7 +44,10 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        if ($exception instanceof CustomException) {
+			return response()->view('maintenance');
+		}
+		//return parent::render($request, $exception);
     }
 
     /**
