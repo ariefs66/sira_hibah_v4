c4ddc86b0dc5d133017d300eb6b512efc1514abd
arief <arief@gmail.com>
2018-03-14 20:16:12 +0700

    maroon

--- resources/views/budgeting/belanja-langsung/akb.blade.php
+++ resources/views/budgeting/belanja-langsung/akb.blade.php
@@ -730,7 +730,8 @@
    total = parseInt(total);
    selisih = total-total_akb;
 
-    if(jan == "" || feb == "" || mar == "" || apr == "" || mei == "" || jun == "" || jul == "" || agu == "" || sep == "" || nov == "" || des == "" ){
+    //if(jan == "" || feb == "" || mar == "" || apr == "" || mei == "" || jun == "" || jul == "" || agu == "" || sep == "" || nov == "" || des == "" ){
+    if(apr == "" || mei == "" || jun == "" || jul == "" || agu == "" || sep == "" || okt == "" || nov == "" || des == "" ){  
       $.alert('Form harap diisi atau di nol kan!');
     }
     else{
