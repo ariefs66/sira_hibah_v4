99c8de56466b8ed80dc793466373cd01d77cf7f3
allrested <allrested@gmail.com>
2018-05-18 14:45:51 +0700

    Fix Statistik Monev

--- app/Http/Controllers/Monev/statistikController.php
+++ app/Http/Controllers/Monev/statistikController.php
@@ -45,7 +45,7 @@ class statistikController extends Controller
                         ->where('BL_TAHUN',$tahun)
                         ->where('BL_DELETED',0)
                         ->where('SKPD_ID',$skpd->SKPD_ID)
-                        ->groupBy('PROGRAM_NAMA',"REF_PROGRAM.PROGRAM_ID","PROGRAM_KODE","OUTCOME_TOLAK_UKUR","OUTCOME_TARGET","SATUAN_NAMA")
+                        ->groupBy('PROGRAM_NAMA',"REF_PROGRAM.PROGRAM_ID","PROGRAM_KODE")
                         ->selectRaw('SUM("BL_PAGU") AS TOTAL, "PROGRAM_NAMA","REF_PROGRAM"."PROGRAM_ID","PROGRAM_KODE","OUTCOME_TOLAK_UKUR","OUTCOME_TARGET","SATUAN_NAMA"')
                         ->get()->count();
           $monev        = Monev_Program::where('DAT_PROGRAM.SKPD_ID',$skpd->SKPD_ID)->where('PROGRAM_TAHUN',$tahun)
