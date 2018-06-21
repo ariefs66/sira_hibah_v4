6b77799567e71a17c35b0092239d6558e87dd758
root <root@sira.frontend>
2018-06-21 23:31:30 +0700

    Aktivasi 2018 Perubahan & 2019

--- app/Http/Controllers/mainController.php
+++ app/Http/Controllers/mainController.php
@@ -65,6 +65,9 @@ class mainController extends Controller
 		
 		foreach($data as $data){
 			 
+			if($data->TAHUN == '2018' && $data->STATUS == 'pergeseran'){
+					$view 	.= '<option value="'.$data->TAHUN.'/'.$data->STATUS.'" id="o'.$data->TAHUN.$data->STATUS.'" selected>'.$data->TAHUN.'-'.$data->STATUS.'</option>';
+}
 			 if($data->TAHUN == '2019'){
 				//if($tahun == $data->TAHUN and $status == $data->STATUS){
 					$view 	.= '<option value="'.$data->TAHUN.'/'.$data->STATUS.'" id="o'.$data->TAHUN.$data->STATUS.'" selected>'.$data->TAHUN.'-'.$data->STATUS.'</option>';
